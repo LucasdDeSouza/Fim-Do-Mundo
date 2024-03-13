c96f1a863d487934de605880c451e5fc29623d90
LucasdDeSouza <lucasdesouzasouza57@gmail.com>
2024-03-13 11:51:44 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { ref } from 'vue';
+const listaCidades = ref( ['São Paulo','Rio de Janeiro', 'Belo Horizonte','Salvador', 'Fortaleza',
+  'Curitiba','Manaus', 'Recife','Porto Alegre','Brasília',] );
+
+function remover(index) {
+    listaCidades.value.splice(index, 1);
+  }
+
+  function adicionar() {
+    listaCidades.value.push(NovaCidade.value)
+    novaCidade.value = ''
+  }
+
+  
+</script>
+
+<template>
+
+   <input type="text" v-model="NovaCidade">
+  <button @click="adicionar">Adicionar</button>
+  <ul>
+    <li v-for="item in listaCidades">{{ item }}</li>
+  </ul>
+
+   <ul>
+    <li v-for="(item, index) in listaCidades " :key="index"> {{ item }} 
+      <button @click="remover(index)">Remover</button></li>
+   </ul>
+  
+</template>
+
+<style scoped>
+
+button{
+  background-color: aqua;
+
+}
+</style>
