ae1eb2587f8fe111b9dd5b549ddfadd615e3e8e5
LucasdDeSouza <lucasdesouzasouza57@gmail.com>
2024-03-15 11:40:26 -0300

    v2

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,57 @@
 <script setup>
-import { ref } from 'vue';
-const listaCidades = ref( ['São Paulo','Rio de Janeiro', 'Belo Horizonte','Salvador', 'Fortaleza',
-  'Curitiba','Manaus', 'Recife','Porto Alegre','Brasília',] );
+import { computed, ref } from 'vue'
+
+const listaCidade = ref(``)
+const listaCidades = ref([
+  'São Paulo',
+  'Rio de Janeiro',
+  'Belo Horizonte',
+  'Salvador',
+  'Fortaleza',
+  'Curitiba',
+  'Manaus',
+  'Recife',
+  'Porto Alegre',
+  'Brasília'
+])
+
+const Ordem = computed(() => [...listaCidades.value].sort())
 
 function remover(index) {
-    listaCidades.value.splice(index, 1);
-  }
+  listaCidades.value.splice(index, 1)
+}
 
-  function adicionar() {
-    listaCidades.value.push(NovaCidade.value)
-    novaCidade.value = ''
-  }
-
-  
+function adicionar() {
+  listaCidades.value.push(listaCidade.value)
+  listaCidade.value = ''
+}
 </script>
 
 <template>
-
-   <input type="text" v-model="NovaCidade">
-  <button @click="adicionar">Adicionar</button>
-  <ul>
-    <li v-for="item in listaCidades">{{ item }}</li>
-  </ul>
-
-   <ul>
-    <li v-for="(item, index) in listaCidades " :key="index"> {{ item }} 
-      <button @click="remover(index)">Remover</button></li>
-   </ul>
-  
+  <h2>Formulário</h2>
+  <div>
+    <input type="text" v-model="listaCidade" />
+    <button @click="adicionar">Adicionar</button>
+  </div>
+  <div class="resul">
+    <div class="cid">
+      <h2>Lista de cidades</h2>
+      <ul>
+        <li v-for="(listaCidade, index) in listaCidades" :key="index">
+          {{ listaCidade }}
+          <button @click="remover(index)">Remover</button>
+        </li>
+      </ul>
+    </div>
+  </div>
+  <div class="cidades">
+    <h2>Cidades ordenadas</h2>
+    <ul>
+      <li v-for="listaCidade in Ordem" :key="listaCidade">
+        {{ listaCidade }}
+      </li>
+    </ul>
+  </div>
 </template>
 
-<style scoped>
-
-button{
-  background-color: aqua;
-
-}
-</style>
+<style scoped></style>
